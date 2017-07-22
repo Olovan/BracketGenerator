0446355258c066fd49c649cf66740f3972c9fee6
Olovan <micahsmith.92@gmail.com>
2017-07-22 19:18:34 -0400

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+		</div> <!-- TextBody -->
+	</div> <!-- table -->
+	<div class="footer">Created by: Micah Smith</div>
+	
+</div> <!-- Table -->
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!--My CSS script-->
+<link rel="stylesheet" href="./main.css">
+</head>
+
+<body>
+<div class="table">
+	<div class="header"> 
+		<div class="title">PHP Projects</div>
+		<a href="index.php">Bracket Generator</a>
+	</div>
+
+	<div class="table">	
+		<div class="textbody">
